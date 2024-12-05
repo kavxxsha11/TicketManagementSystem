bd9c35b9525b60ceb6423bc1208b0c94fb79662a
Kaveesha Rathnayake <kaveesha.20222294@iit.ac.lk>
2024-12-06 00:14:26 +0530

    Update TicketingSystemConfig Class

--- src/config/TicketingSystemConfig.java
+++ src/config/TicketingSystemConfig.java
@@ -1,37 +1,16 @@
 package config;
 
-import java.util.Scanner;
-
 public class TicketingSystemConfig {
     private int totalTickets;
     private int ticketReleaseRate;
     private int customerRetrievalRate;
     private int maxTicketCapacity;
 
-    public void configure() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter Total Tickets: ");
-        totalTickets = getValidInput(scanner);
-
-        System.out.println("Enter Ticket Release Rate: ");
-        ticketReleaseRate = getValidInput(scanner);
-
-        System.out.println("Enter Customer Retrieval Rate: ");
-        customerRetrievalRate = getValidInput(scanner);
-
-        System.out.println("Enter Max Ticket Capacity: ");
-        maxTicketCapacity = getValidInput(scanner);
-
-        System.out.println("Configuration Complete!");
-    }
-
-    private int getValidInput(Scanner scanner) {
-        while (!scanner.hasNextInt()) {
-            System.out.println("Invalid input. Please enter a valid number.");
-            scanner.next();
-        }
-        return scanner.nextInt();
+    public TicketingSystemConfig(int totalTickets, int ticketReleaseRate, int customerRetrievalRate, int maxTicketCapacity) {
+        this.totalTickets = totalTickets;
+        this.ticketReleaseRate = ticketReleaseRate;
+        this.customerRetrievalRate = customerRetrievalRate;
+        this.maxTicketCapacity = maxTicketCapacity;
     }
 
     public int getTotalTickets() {
