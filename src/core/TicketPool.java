3d5c1b27a1460c22a61a6c0c1a4932a2be6961f0
Kaveesha Rathnayake <kaveesha.20222294@iit.ac.lk>
2024-12-05 16:09:28 +0530

    Add addTickets method to TicketPool

--- src/core/TicketPool.java
+++ src/core/TicketPool.java
@@ -1,4 +1,33 @@
 package core;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TicketPool {
+    private Queue<Ticket> ticketQueue;
+    private int maximumCapacity;
+
+    public TicketPool(int maximumCapacity) {
+        this.maximumCapacity = maximumCapacity;
+        this.ticketQueue = new LinkedList<Ticket>();
+    }
+
+    /*
+    addTickets method used by Vendors to add tickets
+     */
+    public synchronized void addTickets(Ticket ticket) {
+        while (ticketQueue.size() >= maximumCapacity) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+
+        this.ticketQueue.add(ticket);
+        notifyAll();
+        System.out.println(Thread.currentThread().getName() + ": Ticket added: " + ticketQueue.size());
+
+    }
 }
