33c66d2d73eb91fb53935c035523d3feb379c667
Kaveesha Rathnayake <kaveesha.20222294@iit.ac.lk>
2024-12-05 16:37:15 +0530

    Add instant variables, getters and setters, and toString method

--- src/core/Ticket.java
+++ src/core/Ticket.java
@@ -1,4 +1,46 @@
 package core;
 
 public class Ticket {
+    private int ticketId;
+    private String title;
+    private int price;
+
+    public Ticket(int ticketId, String title, int price) {
+        this.ticketId = ticketId;
+        this.title = title;
+        this.price = price;
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "ticketId=" + ticketId +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
