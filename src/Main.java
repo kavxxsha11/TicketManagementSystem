a6f64134222eeb5f15c88640454b7c966621e657
Kaveesha Rathnayake <kaveesha.20222294@iit.ac.lk>
2024-12-06 23:27:07 +0530

    Add start, stop, and exit commands

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
 import config.TicketingSystemConfig;
 import ui.CLI;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        TicketingSystemConfig configure = CLI.configure();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("\nCommands:\n \n1. start \n2. stop \n3. exit");
+            String command = scanner.nextLine().trim().toLowerCase();
+
+            switch(command){
+                case "start":
+                    CLI.startSystem();
+                    break;
+                case "stop":
+                    CLI.stopSystem();
+                    break;
+                case "exit":
+                    CLI.stopSystem();
+                    System.out.println("Exiting System...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid command. Please type 'start', 'stop', or 'exit'.'");
+            }
+        }
     }
 }
