f1d7d8681fbc87a8923606f025a969f20f274653
Kaveesha Rathnayake <kaveesha.20222294@iit.ac.lk>
2024-12-06 23:51:46 +0530

    Update Main Class

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        TicketingSystemConfig configure = CLI.configure();
 
-        while (true){
-            System.out.println("\nCommands:\n \n1. start \n2. stop \n3. exit");
-            String command = scanner.nextLine().trim().toLowerCase();
+        try(Scanner scanner = new Scanner(System.in)) {
 
-            switch(command){
-                case "start":
-                    CLI.startSystem();
-                    break;
-                case "stop":
-                    CLI.stopSystem();
-                    break;
-                case "exit":
-                    CLI.stopSystem();
-                    System.out.println("Exiting System...");
-                    scanner.close();
-                    return;
-                default:
-                    System.out.println("Invalid command. Please type 'start', 'stop', or 'exit'.'");
+            while (true) {
+                System.out.println("\nCommands:\n \n1. start \n2. stop \n3. exit");
+                String command = scanner.nextLine().trim().toLowerCase();
+
+                switch (command) {
+                    case "start":
+                        CLI.startSystem();
+                        break;
+                    case "stop":
+                        CLI.stopSystem();
+                        break;
+                    case "exit":
+                        CLI.stopSystem();
+                        System.out.println("Exiting System...");
+                        scanner.close();
+                        return;
+                    default:
+                        System.out.println("Invalid command. Please type 'start', 'stop', or 'exit'.'");
+                }
             }
         }
     }
