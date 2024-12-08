80033988dc5aeb3be1606ee0f048463d273ca25c
Kaveesha Rathnayake <kaveesha.20222294@iit.ac.lk>
2024-12-08 15:33:03 +0530

    Update Customer Class

--- src/threads/Customer.java
+++ src/threads/Customer.java
@@ -3,36 +3,50 @@ package threads;
 import core.Ticket;
 import core.TicketPool;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 // Customer class implements the Runnable interface
 public class Customer implements Runnable {
-    private final TicketPool ticketPool; // Shared ticket pool
-    private final int customerRetrievalRate; // Rate of ticket retrieval
-    private final int amount; // Number of tickets the customer wants to retrieve
+    private static Logger LOGGER = Logger.getLogger(Customer.class.getName());
+    private TicketPool ticketPool; // Shared ticket pool
+    private int customerRetrievalRate; // Rate of ticket retrieval
+    private int amount; // Number of tickets the customer wants to retrieve
+    private String customerName;
+    private int customerID;
 
     // Constructor to initialize the Customer
     public Customer(TicketPool ticketPool, int customerRetrievalRate, int amount) {
         this.ticketPool = ticketPool; // Assign the ticket pool
         this.customerRetrievalRate = customerRetrievalRate; // Set retrieval rate
         this.amount = amount; // Set the number of tickets to retrieve
+        this.customerName = Thread.currentThread().getName();
+        this.customerID = 1;
     }
 
     @Override
     public void run() {
         // Loop to retrieve the specified number of tickets
         for (int i = 0; i < amount; i++) {
-            Ticket ticket = ticketPool.getTicket();
-            if (ticket == null) {
-                System.out.println("No more tickets available for " + Thread.currentThread().getName());
-                break;
+            if (Thread.currentThread().isInterrupted()) {
+                LOGGER.info(Thread.currentThread().getName() + " interrupted. Exiting gracefully. ");
+                return;
             }
-            System.out.println("This ticket purchased by " + Thread.currentThread().getName() + ticket);
 
             try {
+                Ticket ticket = ticketPool.getTicket();
+                if (ticket != null) {
+                    LOGGER.info(Thread.currentThread().getName() + " purchased ticket: " + ticket);
+
+                } else {
+                    LOGGER.warning(Thread.currentThread().getName() + " failed to purchase ticket.");
+                }
                 Thread.sleep(customerRetrievalRate * 1000);
+
             } catch (InterruptedException e) {
-                System.out.println("Customer thread interrupted: " + e.getMessage());
+                LOGGER.warning(Thread.currentThread().getName() + " interrupted while sleeping. Exiting...");
                 Thread.currentThread().interrupt();
-                break;
+                return;
             }
         }
     }
