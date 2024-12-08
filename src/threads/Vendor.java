38e399d2540b38261cf2cca3cbc75ef29466d0a2
Kaveesha Rathnayake <kaveesha.20222294@iit.ac.lk>
2024-12-08 15:36:48 +0530

    Update Vendor Class

--- src/threads/Vendor.java
+++ src/threads/Vendor.java
@@ -3,31 +3,43 @@ package threads;
 import core.Ticket;
 import core.TicketPool;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 // Vendor class implements the Runnable interface
 public class Vendor implements Runnable {
-    private final TicketPool ticketPool; // Shared ticket pool
-    private final int totalTickets; // Total tickets to release
-    private final int ticketReleaseRate; // Ticket releasing rate
+    private static Logger LOGGER = Logger.getLogger(Vendor.class.getName());
+    private TicketPool ticketPool; // Shared ticket pool
+    private int totalTickets; // Total tickets to release
+    private int ticketReleaseRate; // Ticket releasing rate
+    private String vendorName;
+    private int vendorId;
 
     // Constructor to initialize the Vendor
     public Vendor(TicketPool ticketPool, int totalTickets, int ticketReleaseRate) {
         this.ticketPool = ticketPool; // Assign the ticket pool
         this.totalTickets = totalTickets; // Set total tickets
         this.ticketReleaseRate = ticketReleaseRate; // Set release rate
+        this.vendorName = Thread.currentThread().getName();
+        this.vendorId = 1;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < totalTickets; i++) {
-            Ticket ticket = new Ticket(i,"Event",1000.0);
-            ticketPool.addTickets(ticket);
-
+            if (Thread.currentThread().isInterrupted()) {
+                LOGGER.info(Thread.currentThread().getName() + " is interrupted. Exiting gracefully.");
+                return;
+            }
             try {
+                Ticket ticket = new Ticket(i, 1, 1);
+                ticketPool.addTickets(ticket);
+                LOGGER.info(Thread.currentThread().getName() + " added ticket " + ticket);
                 Thread.sleep(ticketReleaseRate * 1000);
             } catch (InterruptedException e) {
-                System.out.println("Vendor thread interrupted " + e.getMessage());
+                LOGGER.warning(Thread.currentThread().getName() + " interrupted while sleeping. Exiting...");
                 Thread.currentThread().interrupt();
-                break;
+                return;
             }
         }
     }
